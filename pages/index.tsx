ae5f657d61ebc9db4c19b79be191eeee0c44c186
Razazele <pablo_verdugoj@hotmail.com>
2023-04-02 10:41:23 -0400

    Added meta - favicon - desc

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import useInfoModalStore from '@/hooks/useInfoModalStore';
 import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next'
 import  {getSession} from 'next-auth/react'
-
+import Head from 'next/head';
 
 //Check if logged in
 export async function getServerSideProps(context:NextPageContext) {
@@ -34,7 +34,14 @@ export default function Home() {
   const {data:favorites} = useFavorites();
   const {isOpen,closeModal} = useInfoModalStore();
   return (
+
     <>
+      <Head>
+        <title>Razmv - Reproductor de series y peliculas</title>
+        <link rel="icon" href='/favicon.ico' />
+        <meta name='description' content='Sitio para ver series y peliculas - Creado por Pablo Verdugo' />
+
+      </Head>
       <InfoModal visible={isOpen} onClose={closeModal}/>
       <Navbar/>
       <Billboard/>
